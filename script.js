24e01c7594202d000e795fa122455fd6c30a6a6b
stephanie-agbo <stephanieagbo1@gmail.com>
2025-05-03 10:19:09 +0100

    v2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// To create the toggle effect we must first write a variable declaration for our document body and for the switch buttom which ww would then add an event listener to so that on click it performs the function we want it to run
+// To create the toggle effect we must first write a variable declaration for our document body and for the switch buttom which we would then add an event listener to so that on click it performs the function we want it to run
 
 //add variable declaration for the button and body
 let themeButton = document.getElementById('theme-switch');
@@ -7,6 +7,18 @@ let body = document.body;
 //Add a click event listener to the button
 themeButton.addEventListener('click', () => 
 {
-//lastly, we tell it what we want it to do by adding a class to the body element
+//lastly, we tell it what we want it to do to the body when the button is clicked
 body.classList.toggle('dark');
+});
+
+//Get the medium button and add an event listener to it
+
+let myarticle = document.getElementById('link');
+
+//add event listener to the variable myarticle
+
+myarticle.addEventListener('click', () =>
+{
+    //when clicked, take user to this article and open in a new tab
+    location.href = 'https://medium.com/@cphanie.as/theme-switch-css-vs-dom-3567c42a7763'; //+ 'target="_blank"';
 });
